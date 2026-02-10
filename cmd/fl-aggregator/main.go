5cb03fab3e4ce7c32a1648daa8b3ebf43af6125a
Ryan <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-02-10 11:44:50 -0800

    Create main.go

---
+++ cmd/fl-aggregator/main.go
@@ -0,0 +1,47 @@
+// cmd/fl-aggregator/main.go
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"math"
+	"net/http"
+)
+
+type GradPayload struct {
+	NodeID string    `json:"node_id"`
+	Grads  []float64 `json:"grads"`
+}
+
+func main() {
+	http.HandleFunc("/fl/submit", handleSubmit)
+	log.Println("FL aggregator on :8090")
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
+
+func handleSubmit(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	var p GradPayload
+	if err := json.Unmarshal(body, &p); err != nil {
+		http.Error(w, "bad json", 400)
+		return
+	}
+
+	// simple clipping
+	maxNorm := 10.0
+	norm := 0.0
+	for _, g := range p.Grads {
+		norm += g * g
+	}
+	norm = math.Sqrt(norm)
+	if norm > maxNorm {
+		scale := maxNorm / norm
+		for i := range p.Grads {
+			p.Grads[i] *= scale
+		}
+	}
+
+	log.Printf("received %d grads from %s (clipped)", len(p.Grads), p.NodeID)
+	w.WriteHeader(200)
+}
