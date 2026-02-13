58089a01eddeba167f1e6a2f0b44ddf1963cf378
Ryan <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-02-13 08:34:40 -0800

    Update main.go

--- cmd/node-agent/main.go
+++ cmd/node-agent/main.go
@@ -1,16 +1,11 @@
-// Copyright 2026 Sovereign-Mohawk Core Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package main
 
 import (
@@ -30,10 +25,10 @@ import (
 func main() {
 	log.Println("Node Agent starting...")
 
-	// Use tpm to satisfy linter
+	// Satisfy tpm import
 	_ = tpm.Verify("node-init", []byte{})
 
-	// Use context and wasmhost to satisfy linter
+	// Satisfy context and wasmhost imports
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
