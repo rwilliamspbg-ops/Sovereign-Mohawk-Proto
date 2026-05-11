2ad0d4c131083c21c093df21707cfd272b8421b8
Ryan <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-05-11 20:00:28 +0000

    test: stub @copilotkit/react-actions locally to enable ops-assistant build

---
+++ web/ops-assistant/local-packages/@copilotkit/react-actions/index.d.ts
@@ -0,0 +1,3 @@
+export function useAction(...args: any[]): any;
+export const ActionsProvider: any;
+export default { useAction: any, ActionsProvider: any };
---
+++ web/ops-assistant/local-packages/@copilotkit/react-actions/index.js
@@ -0,0 +1,10 @@
+// Minimal stub implementation for testing
+exports.__esModule = true;
+exports.useAction = function() {
+  return function noop() { return null; };
+};
+exports.ActionsProvider = function ActionsProvider(_props) { return null; };
+exports.default = {
+  useAction: exports.useAction,
+  ActionsProvider: exports.ActionsProvider
+};
